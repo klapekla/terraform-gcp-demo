37f7944647a5b78777b8266352857f0ad16bf413
Klaus Klatt <kklatt@ovb.de>
2021-03-07 09:47:21 +0100

    add nat for VMs with no public ip

--- 02-base-project/main.tf
+++ 02-base-project/main.tf
@@ -57,6 +57,27 @@ resource "google_compute_subnetwork" "public" {
   network       = google_compute_network.this.id
 }
 
+resource "google_compute_router" "nat" {
+  name    = "my-router"
+  project = google_project.this.project_id
+  region  = var.region
+  network = google_compute_network.this.name
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "my-router-nat"
+  project                            = google_project.this.project_id
+  router                             = google_compute_router.nat.name
+  region                             = google_compute_router.nat.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
 resource "google_compute_firewall" "iap_ingress" {
   name          = "allow-ingress-from-iap"
   project       = google_project.this.project_id
